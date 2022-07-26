46db6290e043595de64b5445045691c7c5526856
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-07-25 22:21:59 -0700

    add routes

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => res.json(note_db));
+
 app.listen(PORT, () => {
     console.log(`Example app listening at https://localhost:${PORT}`);
 });
